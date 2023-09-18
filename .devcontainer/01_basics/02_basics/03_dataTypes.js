2e145754add73fcc6ce879a6ce9629ed4697e46a
Sartaj alam <108234043+Sartajalam1@users.noreply.github.com>
2023-09-18 15:21:07 +0000

    container

---
+++ .devcontainer/01_basics/02_basics/03_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict" // treat all Js  code ae newer version
+let name = "sartaj";
+let age = 18;
+let isLoggedIn = false;
+
+// number => 2 to power 53
+//bigint
+//string
+//Boolean => true/false
+// null => standalone value
+// undefined
+// symbol
+
+//object
+console.log(typeof age);
---
+++ .devcontainer/01_basics/02_basics/variables.js
@@ -0,0 +1,10 @@
+const accountId = 379;
+let acountEmail = "sartaj@gamil.com";
+var accountPassword = "98765";
+let accountCity = " Bettiah";
+accountEmail = "raj@gmail.com";
+let nameId = "sartaj";
+console.log(accountId);
+let accountState;
+console.table([accountId,nameId,accountPassword,accountCity,accountState]);
+console.log(acountEmail);
